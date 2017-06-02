2bbebcd6fc107bd02b9cf8b29f773222a057bb3b
shehzeenhooda <hudashehzeen@gmail.com>
2017-06-02 16:20:57 +0500

    backend functional event system

--- wp-content/themes/council/Register_event_page.php
+++ wp-content/themes/council/Register_event_page.php
@@ -9,59 +9,66 @@
         <div id="primary" class="content-area">
             <main id="main" class="site-main" role="main">
               <div class="container">
+
                 <div class="b-form-row f-primary-l f-title-big c-secondary">Register An Event</div>
                 <hr class="b-hr" />
+                <form id="event_form" name="event_form" method="post" action="<?php echo get_home_url(); ?>/registereventprocessing">
                 <div class="row b-form-inline b-form-horizontal">
                     <div class="col-xs-6">
 
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_name">Event Name</label>
                             <div class="b-form-horizontal__input">
-                                <input type="text" id="event_name" class="form-control" />
+                                <input type="text" name="event_name" id="event_name" class="form-control" />
                             </div>
                         </div>
 
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_date">Event Date</label>
                             <div class="b-form-horizontal__input">
-                                <input type="text" class="form-control datepicker" id="event_date" class="form-control" />
+                                <input type="text" class="form-control datepicker" name="event_date" id="event_date" class="form-control" />
                             </div>
                         </div>
 
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_time">Event Time</label>
                             <div class="b-form-horizontal__input">
-                              <input type="time" id="event_time" class="form-control" />
+                              <input type="time" name="event_time" id="event_time" class="form-control" />
                             </div>
                         </div>
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_area">Event Area</label>
                             <div class="b-form-horizontal__input">
-                                <input type="text" id="event_area" class="form-control" />
+                                <input type="text" name="event_area" id="event_area" class="form-control" />
                             </div>
                         </div>
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_city">Event City</label>
                             <div class="b-form-horizontal__input">
-                                <input type="text" id="event_city" class="form-control" />
+                                <input type="text" name="event_city" id="event_city" class="form-control" />
                             </div>
                         </div>
 
                         <div class="b-form-row">
                             <label class="b-form-horizontal__label" for="event_description">Event Description</label>
                             <div class="b-form-horizontal__input">
-                                <textarea class="form-control" id="event_description" placeholder="Message" rows="5"></textarea>
+                                <textarea class="form-control" name="event_description" id="event_description" placeholder="Message" rows="5"></textarea>
                             </div>
                         </div>
 
                         <div class="b-form-row">
                           <div class="b-form-horizontal__input">
-                            <a href="#" style="text-align:center" class="b-btn f-btn b-btn-md b-btn-default f-primary-b">Register Event</a>
-                        </div>
+                            <input type="submit" class="b-btn f-btn b-btn-md b-btn-default f-primary-b" value="Register Event" tabindex="5" id="event_submit" name="event_submit" class="thread-button" />
+
+        <input type="hidden" name="insert_post" value="post" />
+
+        <?php wp_nonce_field( 'event_form' ); ?>
+</div>
                       </div>
                     </div>
                 </div>
 <br><br>
+</form>
 </div>
             </main>
         </div>
---
+++ wp-content/themes/council/registereventprocessing.php
@@ -0,0 +1,34 @@
+<?php
+/* Template Name: Register event Processing */
+if(is_user_logged_in()==false){ wp_redirect('http://council/login/');}
+get_header(); ?>
+
+<div id="primary">
+    <div id="content" role="main">
+      <?php
+if (isset($_POST['event_submit'])) {
+  //var_dump($_POST);
+$post_information = array(
+    'post_title' => $_POST['event_name'],
+    'post_content' => $_POST['event_description'],
+    'post_type' => 'events',
+    'post_status' => 'publish'
+);
+
+$my_id = wp_insert_post( $post_information );
+var_dump($_POST['event_date']);
+add_post_meta( $my_id, 'event_date', $_POST['event_date'], true );
+add_post_meta( $my_id, 'event_time', $_POST['event_time'], true );
+add_post_meta( $my_id, 'event_area', $_POST['event_area'], true );
+add_post_meta( $my_id, 'event_city', $_POST['event_city'], true );
+add_post_meta( $my_id, 'event_description', $_POST['event_description'], true );
+$url = get_home_url(); ?>
+<div tabindex="1"><b>THANK YOU</b></div>
+<?php } ?>
+</div>
+</div>
+
+
+
+ <?php get_footer();
+?>
