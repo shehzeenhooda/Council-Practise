6d146d7db1ae3a7951de4832b309530960a83556
shehzeenhooda <hudashehzeen@gmail.com>
2017-06-02 16:58:54 +0500

    backend functional event system

---
+++ wp-content/plugins/email.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Email Plugin
+Plugin URI: http://example.com/magic-plugin
+Description: Magic Plugin performs magic
+Version: 2.3
+Author: Mr. Magic
+Author URI: http://example.com/
+*/
+?>
