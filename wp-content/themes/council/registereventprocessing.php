62d6131577114aa97500a0ea00f0b13e6a997494
shehzeenhooda <hudashehzeen@gmail.com>
2017-06-07 12:25:52 +0500

    final all

--- wp-content/themes/council/registereventprocessing.php
+++ wp-content/themes/council/registereventprocessing.php
@@ -16,14 +16,13 @@ $post_information = array(
 );
 
 $my_id = wp_insert_post( $post_information );
-var_dump($_POST['event_date']);
 add_post_meta( $my_id, 'event_date', $_POST['event_date'], true );
 add_post_meta( $my_id, 'event_time', $_POST['event_time'], true );
 add_post_meta( $my_id, 'event_area', $_POST['event_area'], true );
 add_post_meta( $my_id, 'event_city', $_POST['event_city'], true );
 add_post_meta( $my_id, 'event_description', $_POST['event_description'], true );
 $url = get_home_url(); ?>
-<div tabindex="1"><b>THANK YOU</b></div>
+<div tabindex="1"><br><h3 align="center">THANK YOU</h3><br></div>
 <?php } ?>
 </div>
 </div>
