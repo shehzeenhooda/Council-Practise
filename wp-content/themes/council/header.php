9b758d60315696a04c18b7c5dfc7057674dc9de5
shehzeenhooda <hudashehzeen@gmail.com>
2017-06-07 11:47:56 +0500

    ammedment

--- wp-content/themes/council/header.php
+++ wp-content/themes/council/header.php
@@ -35,7 +35,7 @@
 <link type="text/css" rel='stylesheet' href="<?php bloginfo("template_url"); ?>/js/fancybox/jquery.fancybox.css">
 <!-- End Fancybox -->
 
-//<link type="text/css" rel='stylesheet' href="<?php bloginfo("template_url"); ?>/fonts/fonts.css">
+<link type="text/css" rel='stylesheet' href="<?php bloginfo("template_url"); ?>/fonts/fonts.css">
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
